1a0e059da801124ef9c1b265d48752eb39588ad6
GAierken <g.aierken@gmail.com>
2020-09-22 22:38:25 -0400

    first attempt with api

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,29 +1,43 @@
 import React, { Component } from 'react'
 import { Menu } from 'semantic-ui-react'
+const apiUrl = "http://api.openweathermap.org/data/2.5/"
+const api = process.env.REACT_APP_WEATHER_API_KEY
 
-export default class MenuExampleVerticalSecondary extends Component {
+export default class sideMenu extends Component {
+  
+
+
   state = { activeItem: 'New York, NY' }
 
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+  handleItemClick = (e, { name }) => {
+    this.setState({ activeItem: name })
+    console.log(name)
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${name}&APPID=b189bd79d077113ba7f92cc757207908`)
+    .then(r => r.json())
+    .then(data => {
+      console.log(data.weather)
+    })
+  }
+  
 
   render() {
     const { activeItem } = this.state
-
+   
     return (
       <Menu pointing secondary vertical inverted color="violet">
         <Menu.Item
-          name='New York, NY'
-          active={activeItem === 'New York, NY'}
+          name='New York'
+          active={activeItem === 'New York'}
           onClick={this.handleItemClick}
         />
         <Menu.Item
-          name='Los Angeles, CA'
-          active={activeItem === 'Los Angeles, CA'}
+          name='Los Angeles'
+          active={activeItem === 'Los Angeles'}
           onClick={this.handleItemClick}
         />
         <Menu.Item
-          name='Miami, FL'
-          active={activeItem === 'Miami, FL'}
+          name='Miami'
+          active={activeItem === 'Miami'}
           onClick={this.handleItemClick}
         />
          <Menu.Item
