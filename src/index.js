f01fdffafccfc4d77563c3c996659ddc1b4d5d8e
GAierken <g.aierken@gmail.com>
2020-09-25 20:24:16 -0400

    trying thunk to work then add saga

--- src/index.js
+++ src/index.js
@@ -7,28 +7,28 @@ import 'semantic-ui-css/semantic.min.css'
 //redux
 import { Provider } from 'react-redux'
 import weatherReducer from './redux/reducer'
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk'
 //redux saga
-import createSagaMiddleware from 'redux-saga'
+// import createSagaMiddleware from 'redux-saga'
 
 
-import {helloSaga} from './redux/saga'
+// import {helloSaga} from './redux/saga'
 //router
 import { BrowserRouter as Router} from 'react-router-dom'
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 
-
-const sagaMiddleware = createSagaMiddleware();
-console.log(sagaMiddleware())
-
+// const sagaMiddleware = createSagaMiddleware();
+// console.log(sagaMiddleware)
 const store = createStore(
   weatherReducer, 
-  applyMiddleware(sagaMiddleWare)
+  composeEnhancers(applyMiddleware(thunk))
   )
 
 
-
+// sagaMiddleware.run(helloSaga)
 
 ReactDOM.render(
   
