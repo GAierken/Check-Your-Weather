661e398c77651e5fe5a828403e89b1db3c6d2bbd
GAierken <g.aierken@gmail.com>
2020-09-26 22:01:49 -0400

    added input and city search is working

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Menu, Search } from 'semantic-ui-react'
+import { Menu, Input, Button } from 'semantic-ui-react'
 import Geocode from "react-geocode"
 import { connect } from 'react-redux'
 import { requestApiData, requestActiveItemData } from "../redux/actions"
@@ -9,7 +9,9 @@ Geocode.setApiKey(process.env.REACT_APP_MAP_API_KEY)
 
  class SideMenu extends Component {
   
-
+        state ={
+          searchValue: ""
+        }
 
  
 
@@ -30,7 +32,33 @@ Geocode.setApiKey(process.env.REACT_APP_MAP_API_KEY)
   })
 
    
+
   }
+
+ handleOnChange = (e) => {
+    console.log(e.target.value)
+    this.setState({
+      searchValue: e.target.value.toLowerCase()
+    })
+ }
+
+ handleClick = () => {
+   let name = this.state.searchValue
+    
+   this.props.requestActiveItemData({name})
+
+   Geocode.fromAddress(name)
+        .then(r => {
+            const {lat, lng} = r.results[0].geometry.location
+           
+             this.props.requestApiData({lat, lng});
+            
+  },
+  error => {
+    console.error(error)
+  })
+    
+ }
 
   render() {
     const { activeItem } = this.props
@@ -54,7 +82,10 @@ Geocode.setApiKey(process.env.REACT_APP_MAP_API_KEY)
           onClick={this.handleItemClick}
         />
          <Menu.Item>
-            <Search size='mini'/>
+         <Input type='text' placeholder='Search...' action>
+          <Input size="mini" onChange={this.handleOnChange}/>
+          <Button type='submit' size="mini" onClick={this.handleClick}>Search</Button>
+        </Input>
          </Menu.Item>
 
       </Menu>
