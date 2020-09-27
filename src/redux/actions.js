80c2091249e71c9bc0ba6421fbab37b66c7fa620
GAierken <g.aierken@gmail.com>
2020-09-26 21:03:42 -0400

    activeItem works

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -2,5 +2,11 @@
 export const REQUEST_API_DATA = "REQUEST_API_DATA";
 export const RECEIVE_API_DATA = "RECEIVE_API_DATA";
 
+export const REQUEST_ACTIVEITEM_DATA = "REQUEST_ACTIVEITEM_DATA"
+export const RECEIVE_ACTIVEITEM_DATA = "RECEIVE_ACTIVEITEM_DATA";
+
 export const requestApiData = data => ({ type: REQUEST_API_DATA, data });
 export const receiveApiData = data => ({ type: RECEIVE_API_DATA, data });
+
+export const requestActiveItemData = data => ({ type: REQUEST_ACTIVEITEM_DATA, data });
+export const receiveActiveItemData = data => ({ type: RECEIVE_ACTIVEITEM_DATA, data });
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,18 +1,27 @@
 const initialState = {
-    weathers: []
+    weathers: [],
+    activeItem: 'New York'
+    // activeItem: 'New York'
 }
 
 const weatherReducer = (state = initialState, action) => {
-       
+       console.log()
         switch(action.type){
            case "RECEIVE_API_DATA":
                return{
                    ...state,
                    weathers: action.data
                }
+           case "RECEIVE_ACTIVEITEM_DATA":
+               return{
+                   ...state,
+                   activeItem: action.data.name
+               }
             default:
                 return state
         }
+
+     
 }
 
 
