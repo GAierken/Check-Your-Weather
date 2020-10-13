d2fbe493a8a5ec4d94c83e94544cce164c77f467
GAierken <g.aierken@gmail.com>
2020-10-12 21:36:10 -0400

    added second parameters for useEffect

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -87,7 +87,7 @@ Geocode.setApiKey(process.env.REACT_APP_MAP_API_KEY)
 
   
     return (
-      <Menu  size="huge" pointing secondary vertical inverted color="violet" style={{'marginTop': '100px'}}>
+      <Menu  size="huge" pointing secondary vertical inverted color="violet" style={{marginTop: '100px'}}>
         <Menu.Item
           name='New York'
           active={activeItem === 'New York'}
